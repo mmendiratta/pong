b619b07b00917c280b8e8f28f473c176bb980869
Manav Mendiratta <mmendiratta@wisc.edu>
2018-10-26 18:15:42 -0500

    Adding the first code that was made in Unity

---
+++ Assets/BallBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour {
+    public int velx;
+    public int vely;
+    int posx;
+    int posy;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        posx += velx;
+        posy += vely;
+	}
+}
